438739265b366552c6afb68d8c38e4408f3c797a
Petkub <63050140@kmitl.ac.th>
2022-11-09 18:22:40 +0700

    Final
    พอละไอเหี้ย

--- functions/index.js
+++ functions/index.js
@@ -37,10 +37,110 @@ exports.webhook = functions
     const agent = new WebhookClient({ request: req, response: res });
 
     const gameSuggestion = async agent => {
-          agent.add("Here are some games you might like!");
+      var randGame = Array.from(myRandomInt(3, 10));
+      var gameList = ['Adventure', 'Arcade', 'FPS', 'Horor', 'MMO', 'MOBA', 'Puzzle', 'Racing', 'Sport', 'Survival'];
+      var price = ['ถูกๆ', 200, 400, 600, 800, 1000];
+      var randPrice = Array.from(myRandomInt(3, 5));
+      let flexMsg = {
+        "line": {
+          "type": "flex",
+          "altText": "Flex Message",
+          "contents": {
+            "type": "bubble",
+            "direction": "ltr",
+            "hero": {
+              "type": "image",
+              "url": "https://wallpapers.com/images/high/minecraft-meme-steve-and-creeper-ymxwe6ty97hvobk0-ymxwe6ty97hvobk0.webp",
+              "size": "full",
+              "aspectRatio": "1.51:1",
+              "aspectMode": "cover"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "เกมแนวไหนดี?",
+                  "weight": "bold",
+                  "align": "center",
+                  "contents": []
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "horizontal",
+              "contents": [
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "message",
+                        "label": gameList[randGame[0]] + " " + price[randPrice[0]],
+                        "text": gameList[randGame[0]] + " " + price[randPrice[0]]
+                      },
+                      "color": "#55EA3456",
+                      "style": "secondary",
+                      "offsetEnd": "xs"
+                    },
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "message",
+                        "label": gameList[randGame[1]] + " " + price[randPrice[1]],
+                        "text": gameList[randGame[1]] + " " + price[randPrice[1]]
+                      },
+                      "color": "#55EA3456",
+                      "style": "secondary",
+                      "offsetTop": "sm",
+                      "offsetEnd": "xs"
+                    }
+                  ]
+                },
+                {
+                  "type": "box",
+                  "layout": "vertical",
+                  "contents": [
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "message",
+                        "label": gameList[randGame[2]] + " " + price[randPrice[2]],
+                        "text": gameList[randGame[2]] + " " + price[randPrice[2]]
+                      },
+                      "color": "#55EA3456",
+                      "style": "secondary",
+                      "offsetStart": "xs"
+                    },
+                    {
+                      "type": "button",
+                      "action": {
+                        "type": "message",
+                        "label": "อะไรก็ได้",
+                        "text": "อะไรก็ได้"
+                      },
+                      "color": "#FFFFFFFF",
+                      "style": "secondary",
+                      "offsetTop": "sm",
+                      "offsetStart": "xs"
+                    }
+                  ]
+                }
+              ]
+            }
+          }
+        }
+          }
+            const payload = new Payload("LINE", flexMsg, {rawPayload: true, sendAsMessage: true });
+            agent.add(payload);
     };
 
     const gameSuggestionCategory = async agent => {
+      const text = req.body.queryResult.queryText;
       let flexMsg = {
         "line": {
           "type": "flex",
@@ -50,6 +150,7 @@ exports.webhook = functions
             "contents": [
             ]
           }
+          
         }
       };
 
@@ -63,7 +164,7 @@ exports.webhook = functions
       var fixPrice= req.body.queryResult.parameters.price;
       var condition = req.body.queryResult.parameters.condition;
       
-      if(fixPrice != null && price.length == 0)
+      if(fixPrice != "")
       {
         if(fixPrice == "Free")
           var priceRef = gameRef.where("price", "==", 0);
@@ -109,9 +210,9 @@ exports.webhook = functions
           data_pack.push(doc.data());
         });
         let rand = [];
-        let bubbleFlex = 3;
+        let bubbleFlex = 4;
 
-        if(data_pack.length < 3)
+        if(data_pack.length < bubbleFlex)
         {
           bubbleFlex = data_pack.length;
         }
@@ -141,6 +242,7 @@ exports.webhook = functions
                   "text": data_pack[rand[i]].game,
                   "weight": "bold",
                   "size": "xl",
+                  "gravity": "center",
                   "contents": []
                 },
                 {
@@ -155,15 +257,14 @@ exports.webhook = functions
                         {
                           "type": "icon",
                           "url": "https://mario.wiki.gallery/images/thumb/1/17/CoinMK8.png/1200px-CoinMK8.png",
-                          "size": "xxl"
+                          "size": "xl"
                         },
                         {
                           "type": "text",
                           "text": `${data_pack[rand[i]].price} บาท`,
                           "weight": "bold",
                           "size": "xxl",
-                          "margin": "lg",
-                          "offsetBottom": "sm",
+                          "margin": "sm",
                           "contents": []
                         }
                       ]
@@ -172,10 +273,9 @@ exports.webhook = functions
                 },
                 {
                   "type": "text",
-                  "text": "Release date: " + data_pack[rand[i]].releaseDate,
-                  "weight": "regular",
-                  "size": "md",
-                  "color": "#574E4EFF",
+                  "text": "Release Date:" + data_pack[rand[i]].releaseDate,
+                  "size": "xs",
+                  "color": "#000000FF",
                   "wrap": true,
                   "contents": []
                 }
@@ -186,17 +286,13 @@ exports.webhook = functions
               "layout": "vertical",
               "contents": [
                 {
-                  "type": "spacer",
-                  "size": "xs"
-                },
-                {
                   "type": "button",
                   "action": {
                     "type": "uri",
                     "label": "Website",
                     "uri": data_pack[rand[i]].link
                   },
-                  "color": "#1B8DC5FF",
+                  "color": "#905C44",
                   "style": "primary"
                 }
               ]
@@ -204,18 +300,114 @@ exports.webhook = functions
           }
           flexMsg.line.contents.contents.push(gameData);
         }
+          let dataEnd = {
+            "type": "bubble",
+            "hero": {
+              "type": "image",
+              "url": "https://wallpapercave.com/wp/wp8422268.jpg",
+              "size": "full",
+              "aspectRatio": "20:13",
+              "aspectMode": "cover"
+            },
+            "body": {
+              "type": "box",
+              "layout": "vertical",
+              "spacing": "md",
+              "contents": [
+                {
+                  "type": "text",
+                  "text": "ต้องการเกมเพิ่มไหม?",
+                  "weight": "bold",
+                  "size": "xl",
+                  "align": "center",
+                  "gravity": "center",
+                  "contents": []
+                }
+              ]
+            },
+            "footer": {
+              "type": "box",
+              "layout": "vertical",
+              "contents": [
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "message",
+                    "label": "More",
+                    "text": text
+                  },
+                  "color": "#5BDF9EFF",
+                  "style": "primary"
+                },
+                {
+                  "type": "button",
+                  "action": {
+                    "type": "message",
+                    "label": "พอแล้ว",
+                    "text": "พอแล้ว"
+                  },
+                  "color": "#FFFFFFFF",
+                  "style": "secondary"
+                }
+              ]
+            }
+          }
+          flexMsg.line.contents.contents.push(dataEnd);
         if(bubbleFlex > 0)
         {
-          const payloadFlexMsg = new Payload("LINE", flexMsg, {rawPayload: true, sendAsMessage: true });
-          agent.add(payloadFlexMsg);
+          const payloadMsg = new Payload("LINE", flexMsg, {rawPayload: true, sendAsMessage: true})
+          agent.add(payloadMsg);
         }
         else
+        {
           agent.add("ไม่พบข้อมูล");
+        }
       });
+    };
 
-  };
+    const gameSuggestionNo = async agent => {
+      var setRandom = myRandomInt(3, 10);
+      var rand = Array.from(setRandom);
+      var gameList = ['Adventure', 'Arcade', 'FPS', 'Horor', 'MMO', 'MOBA', 'Puzzle', 'Racing', 'Sport', 'Survival'];
+      let msgQuickReply = {
+        "line": {
+          "type": "text",
+          "text": "ไว้เจอกันใหม่ ลาก่อน",
+          "quickReply": {
+            "items": [
+              {
+                "type": "action",
+                  "action": {
+                  "type": "message",
+                  "label": "อยากเล่นเกม",
+                  "text": "อยากเล่นเกม"
+                },
+              },
+            ]
+          }
+        }
+      }
+      for (let i = 0; i < 3; i++) 
+      {
+        let itemData =
+          {
+            "type": "action",
+              "action": {
+              "type": "message",
+              "label": gameList[rand[i]],
+              "text": gameList[rand[i]]
+            }
+          }
+        msgQuickReply.line.quickReply.items.push(itemData);
+      }
+      const payloadMsg = new Payload("LINE", msgQuickReply, {rawPayload: true, sendAsMessage: true});
+      agent.add(payloadMsg);
+      //agent.add("ไว้เจอกันใหม่ ลาก่อน");
+    };
+
     let intentMap = new Map();
     intentMap.set("game-suggestion", gameSuggestion);
     intentMap.set("game-suggestion-category", gameSuggestionCategory);
+    intentMap.set("game-suggestion-category-no", gameSuggestionNo);
     agent.handleRequest(intentMap);
   });
